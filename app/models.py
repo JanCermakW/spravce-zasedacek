7306ff2ff90ecc37251f31efe347668935df0de0
JanCermakW <honzacermak74@gmail.com>
2026-02-18 11:01:23 +0100

    impl uživatele a refaktor kodu s tím spojený

--- app/models.py
+++ app/models.py
@@ -10,7 +10,12 @@ class Room(SQLModel, table=True):
 class Booking(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     room_id: int = Field(foreign_key="room.id")
-    user_name: str
+    user_id: int = Field(foreign_key="user.id")
     start_time: datetime
     end_time: datetime
     attendees: int
+
+class User(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    username: str
+    email: str
